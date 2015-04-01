c86830a81ef7246cb749f7c983e9c5f8646ef743
Daniel Rakhamimov <danielrakh@gmail.com>
2015-04-01 12:51:47 -0400

    Add HNPost Model

---
+++ HackerNews/HNPost.m
@@ -0,0 +1,13 @@
+//
+//  HNPost.m
+//  HackerNews
+//
+//  Created by Daniel on 4/1/15.
+//  Copyright (c) 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+#import "HNPost.h"
+
+@implementation HNPost
+
+@end
