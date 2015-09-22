f8395fc35f17fe189834ba37b697d592fc5a1e92
Daniel Rakhamimov <danielrakh@gmail.com>
2015-09-22 13:06:00 -0400

    Update Pods

---
+++ Pods/Target Support Files/Firebase-RACExtensions/Firebase-RACExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Firebase_RACExtensions : NSObject
+@end
+@implementation PodsDummy_Firebase_RACExtensions
+@end
---
+++ Pods/Target Support Files/ReactiveCoreData/ReactiveCoreData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ReactiveCoreData : NSObject
+@end
+@implementation PodsDummy_ReactiveCoreData
+@end
