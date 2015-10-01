0560d39a1cbbcaff111880f234557027bd61aff1
Daniel Rakhamimov <danielrakh@gmail.com>
2015-10-01 17:40:03 -0400

    Comment Text View Styling

--- HackerNews/HNCommentsRootCell.m
+++ HackerNews/HNCommentsRootCell.m
@@ -7,18 +7,28 @@
 //
 
 #import "HNCommentsRootCell.h"
+#import "UIColor+HNColorPalette.h"
 
 @implementation HNCommentsRootCell
 
 - (void)awakeFromNib {
     // Initialization code
-    self.commentTextView.scrollEnabled = NO; 
+    [super awakeFromNib];
+    [self setupCommentTextView];
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (void)setupCommentTextView {
+    self.commentTextView.backgroundColor = [UIColor lightGrayColor];
+    self.commentTextView.clipsToBounds = NO;
+    self.commentTextView.editable = NO;
+    self.commentTextView.linkTextAttributes = @{NSForegroundColorAttributeName : [UIColor HNOrange]};
+    self.commentTextView.selectable = YES;
+    self.commentTextView.dataDetectorTypes = UIDataDetectorTypeLink;
+    self.commentTextView.scrollEnabled = NO;
+    self.commentTextView.textContainer.lineBreakMode = NSLineBreakByWordWrapping;
+    self.commentTextView.textContainer.lineFragmentPadding = 0;
+    self.commentTextView.textContainerInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    self.commentTextView.textAlignment = NSTextAlignmentLeft;
+    
 }
-
 @end
